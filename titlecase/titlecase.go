be0de3058d60414cfebc6fb5405aad7a9ffdb017
KenjiTakahashi <wozniakk@gmail.com>
2014-08-10 23:57:08 +0200

    titlecase: some linting and simplifing

--- titlecase/titlecase.go
+++ titlecase/titlecase.go
@@ -38,30 +38,29 @@ import (
 )
 
 var (
-	SMALL = `a|an|and|as|at|but|by|en|for|if|in|of|on|or|the|to|v\.?|via|vs\.?`
-	PUNCT = "!\"#$%&'‘()*+,\\-./:;?@[\\]_`{|}~"
+	rSmall = `a|an|and|as|at|but|by|en|for|if|in|of|on|or|the|to|v\.?|via|vs\.?`
+	rPunct = "!\"#$%&'‘()*+,\\-./:;?@[\\]_`{|}~"
 
-	SMALL_WORDS   = regexp.MustCompile(fmt.Sprintf(`(?i)^(%s)$`, SMALL))
-	INLINE_PERIOD = regexp.MustCompile(`(?i)[a-z][.][a-z]`)
-	UC_ELSEWHERE  = regexp.MustCompile(fmt.Sprintf(`[%s]*?[a-zA-Z]+[A-Z]+?`, PUNCT))
-	CAPFIRST      = regexp.MustCompile(fmt.Sprintf(`^[%s]*?([\p{L}])`, PUNCT))
-	SMALL_FIRST   = regexp.MustCompile(fmt.Sprintf(`(?i)^([%s]*)(%s)\b`, PUNCT, SMALL))
-	SMALL_LAST    = regexp.MustCompile(fmt.Sprintf(`(?i)\b(%s)[%s]?$`, SMALL, PUNCT))
-	SUBPHRASE     = regexp.MustCompile(fmt.Sprintf(`([:.;?!][ ])(%s)`, SMALL))
-	APOS_SECOND   = regexp.MustCompile(`(?i)^[dol]{1}['‘]{1}[a-z]+$`)
-	ALL_CAPS      = regexp.MustCompile(fmt.Sprintf(`^[A-Z\s\d%s]+$`, PUNCT))
-	UC_INITIALS   = regexp.MustCompile(`^(?:[A-Z]{1}\.{1}|[A-Z]{1}\.{1}[A-Z]{1})+$`)
-	MAC_MC        = regexp.MustCompile(`^([Mm]c)(\w+)`)
+	rSmallWords   = regexp.MustCompile(fmt.Sprintf(`(?i)^(%s)$`, rSmall))
+	rInlinePeriod = regexp.MustCompile(`(?i)[a-z][.][a-z]`)
+	rUCElsewhere  = regexp.MustCompile(fmt.Sprintf(`[%s]*?[a-zA-Z]+[A-Z]+?`, rPunct))
+	rCapFirst     = regexp.MustCompile(fmt.Sprintf(`^[%s]*?([\p{L}])`, rPunct))
+	rSmallFirst   = regexp.MustCompile(fmt.Sprintf(`(?i)^([%s]*)(%s)\b`, rPunct, rSmall))
+	rSmallLast    = regexp.MustCompile(fmt.Sprintf(`(?i)\b(%s)[%s]?$`, rSmall, rPunct))
+	rSubPhrase    = regexp.MustCompile(fmt.Sprintf(`([:.;?!][ ])(%s)`, rSmall))
+	rAposSecond   = regexp.MustCompile(`(?i)^[dol]{1}['‘]{1}[a-z]+$`)
+	rAllCaps      = regexp.MustCompile(fmt.Sprintf(`^[A-Z\s\d%s]+$`, rPunct))
+	rUCInitials   = regexp.MustCompile(`^(?:[A-Z]{1}\.{1}|[A-Z]{1}\.{1}[A-Z]{1})+$`)
 
-	LINES = regexp.MustCompile(`[\r\n]+`)
-	WORDS = regexp.MustCompile(`[\t ]`)
+	rLines = regexp.MustCompile(`[\r\n]+`)
+	rWords = regexp.MustCompile(`[\t ]`)
 )
 
 // PreHook defines Convert per hook function signature
-type PreHook func(word string, all_caps bool) (string, bool)
+type PreHook func(word string, allCaps bool) (string, bool)
 
 // PostHook defines Convert post hook function signature
-type PostHook func(word string, all_caps bool) string
+type PostHook func(word string, allCaps bool) string
 
 // Convert changes input string to conform to the NY Times Manual of Style.
 //
@@ -71,65 +70,61 @@ type PostHook func(word string, all_caps bool) string
 // Both hook functions should return a transformed version of the string.
 // Additionally, pre_hook returns true if the returned string is final,
 // or false if it should still be run through standard transformations.
-func Convert(text string, pre_hook PreHook, post_hook PostHook) string {
-	input := LINES.Split(text, -1)
+func Convert(text string, preHook PreHook, postHook PostHook) string {
+	input := rLines.Split(text, -1)
 	output := make([]string, len(input))
 
 	for i, line := range input {
-		all_caps := ALL_CAPS.MatchString(line)
-		words := WORDS.Split(line, -1)
-		tc_line := make([]string, len(words))
+		allCaps := rAllCaps.MatchString(line)
+		words := rWords.Split(line, -1)
+		tcLine := make([]string, len(words))
 
 		for j, word := range words {
-			if pre_hook != nil {
+			if preHook != nil {
 				stop := false
-				word, stop = pre_hook(word, all_caps)
+				word, stop = preHook(word, allCaps)
 				if stop {
-					tc_line[j] = word
+					tcLine[j] = word
 					continue
 				}
 			}
 
-			if all_caps {
-				if UC_INITIALS.MatchString(word) {
-					tc_line[j] = word
+			if allCaps {
+				if rUCInitials.MatchString(word) {
+					tcLine[j] = word
 					continue
 				}
 				word = strings.ToLower(word)
 			}
 
-			if APOS_SECOND.MatchString(word) {
-				tc_line[j] = APOS_SECOND.ReplaceAllStringFunc(word, strings.Title)
-			} else if INLINE_PERIOD.MatchString(word) || UC_ELSEWHERE.MatchString(word) {
-				tc_line[j] = word
-			} else if SMALL_WORDS.MatchString(word) {
-				tc_line[j] = strings.ToLower(word)
-			} else if match := MAC_MC.FindStringSubmatch(word); match != nil {
-				tc_line[j] = fmt.Sprintf(
-					"%s%s", strings.Title(match[1]), strings.Title(match[2]),
-				)
+			if rAposSecond.MatchString(word) {
+				tcLine[j] = rAposSecond.ReplaceAllStringFunc(word, strings.Title)
+			} else if rInlinePeriod.MatchString(word) || rUCElsewhere.MatchString(word) {
+				tcLine[j] = word
+			} else if rSmallWords.MatchString(word) {
+				tcLine[j] = strings.ToLower(word)
 			} else {
 				sep := "-"
 				if strings.Contains(word, "/") && !strings.Contains(word, "//") {
 					sep = "/"
 				}
-				sep_split := strings.Split(word, sep)
-				cap_first := make([]string, len(sep_split))
-				for k, ss := range sep_split {
-					cap_first[k] = CAPFIRST.ReplaceAllStringFunc(ss, strings.ToTitle)
+				sepSplit := strings.Split(word, sep)
+				capFirst := make([]string, len(sepSplit))
+				for k, ss := range sepSplit {
+					capFirst[k] = rCapFirst.ReplaceAllStringFunc(ss, strings.ToTitle)
 				}
-				tc_line[j] = strings.Join(cap_first, sep)
+				tcLine[j] = strings.Join(capFirst, sep)
 			}
 
-			if post_hook != nil {
-				tc_line[j] = post_hook(tc_line[j], all_caps)
+			if postHook != nil {
+				tcLine[j] = postHook(tcLine[j], allCaps)
 			}
 		}
 
-		result := strings.Join(tc_line, " ")
-		result = SMALL_FIRST.ReplaceAllStringFunc(result, strings.Title)
-		result = SMALL_LAST.ReplaceAllStringFunc(result, strings.Title)
-		result = SUBPHRASE.ReplaceAllStringFunc(result, strings.Title)
+		result := strings.Join(tcLine, " ")
+		result = rSmallFirst.ReplaceAllStringFunc(result, strings.Title)
+		result = rSmallLast.ReplaceAllStringFunc(result, strings.Title)
+		result = rSubPhrase.ReplaceAllStringFunc(result, strings.Title)
 
 		output[i] = result
 	}
